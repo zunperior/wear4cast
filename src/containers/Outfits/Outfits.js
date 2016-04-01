4d544639590978ade34e5caceb8d041c420d98de
zunperior <telepet9@gmail.com>
2016-04-01 17:06:01 -0400

    missed file

--- src/containers/Outfits/Outfits.js
+++ src/containers/Outfits/Outfits.js
@@ -3,8 +3,8 @@ import Helmet from 'react-helmet';
 import {connect} from 'react-redux';
 import { asyncConnect } from 'redux-async-connect';
 import {isLoaded, load as loadOutfits, changeOutfit} from 'redux/modules/outfits';
-
-import  {CurrentOutfit} from 'containers';
+import {CurrentOutfit, Forecast} from 'containers';
+import {ClothingItemList as ClothingItems} from 'components';
 
 function getNextIndex(currentIndex, arrayLength){
   const nextIndex = currentIndex + 1;
@@ -59,19 +59,19 @@ export default class Outfits extends Component {
     changeOutfit: PropTypes.func.isRequired
   };
 
-  // componentDidMount(){
-  //   const {outfits, selectedOutfitIndex, changeOutfit} = this.props;
-  //   setTimeout(() => selectNextOutfit(outfits, selectedOutfitIndex, changeOutfit), 5000);
-  // }
+  componentDidMount(){
+    const {outfits, selectedOutfitIndex, changeOutfit} = this.props;
+    setTimeout(() => selectNextOutfit(outfits, selectedOutfitIndex, changeOutfit), 30000);
+  }
 
  componentDidUpdate()
  {
    const {outfits, selectedOutfitIndex, changeOutfit} = this.props;
-   setTimeout(() => selectNextOutfit(outfits, selectedOutfitIndex, changeOutfit), 5000);
+   setTimeout(() => selectNextOutfit(outfits, selectedOutfitIndex, changeOutfit), 30000);
  }
 
   render() {
-    const {loading, outfits} = this.props;
+    const {loading, outfits, currentOutfit} = this.props;
 
     let refreshClassName = 'fa fa-refresh';
     if (loading) {
@@ -84,14 +84,19 @@ export default class Outfits extends Component {
 			<div className={styles.outfits + ' container'}>
 				<h1>{'Outfits'}</h1>
 				<Helmet title="Outfits"/>
-
-
         <div>
           {
             outfits && outfits.length &&
             <CurrentOutfit />
           }
         </div>
+        <div>
+          {
+            outfits && outfits.length && currentOutfit &&
+            <ClothingItems clothingItemList = {currentOutfit.items} />
+          }
+        </div>
+        <Forecast />
 			</div>
 			);
   }
