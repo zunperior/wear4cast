4869b7e8550495ba9aa7de5d8650af4b477d479b
zunperior <telepet9@gmail.com>
2016-04-16 14:49:20 -0400

    Fixed outfits loading based on weather

--- src/containers/CurrentWeather/CurrentWeather.js
+++ src/containers/CurrentWeather/CurrentWeather.js
@@ -16,6 +16,76 @@ function localDateTime(unixDateTime, timezone){
   return localTime;
 }
 
+function simplifyWeatherConditionId(weatherConditionId)
+{
+
+  const id = ''+ weatherConditionId;
+  switch (id) {
+    case '200':	//thunderstorm with light rain
+    case '201':	//thunderstorm with rain
+    case '202':	//thunderstorm with heavy rain
+    case '210':	//light thunderstorm
+    case '211':	//thunderstorm
+    case '212':	//heavy thunderstorm
+    case '221':	//ragged thunderstorm
+    case '230':	//thunderstorm with light drizzle
+    case '231':	//thunderstorm with drizzle
+    case '232':	//thunderstorm with heavy drizzle
+    case '300':	//light intensity drizzle
+    case '301':	//drizzle
+    case '302':	//heavy intensity drizzle
+    case '310':	//light intensity drizzle rain
+    case '311':	//drizzle rain
+    case '312':	//heavy intensity drizzle rain
+    case '313':	//shower rain and drizzle
+    case '314':	//heavy shower rain and drizzle
+    case '321': //shower drizzle
+    case '500':	//light rain
+    case '501':	//moderate rain
+    case '502':	//very heavy rain
+    case '504':	//extreme rain
+    case '511':	//freezing rain
+    case '520':	//light intensity shower rain
+    case '521':	//shower rain
+    case '522':	//heavy intensity shower rain
+    case '531':	//ragged shower rain
+    case '900':	//tornado
+    case '901':	//tropical storm
+    case '902':	//hurricane
+    case '906':	//hail
+    case '960':	//storm
+    case '961':	//violent storm
+    case '962':	//hurricane
+      return 'RAIN';
+
+    case '600':	//light snow
+    case '601':	//snow
+    case '602':	//heavy snow
+    case '611':	//sleet
+    case '612':	//shower sleet
+    case '615':	//light rain and snow
+    case '616':	//rain and snow
+    case '620':	//light shower snow
+    case '621':	//shower snow
+    case '622':	//heavy shower snow
+      return 'SNOW';
+
+    case '781':	//tornado
+    case '905':	//windy
+    case '954':	//moderate breeze
+    case '955':	//fresh breeze
+    case '956':	//strong breeze
+    case '957':	//high wind, near gale
+    case '958':	//gale
+    case '959':	//severe gale
+      return 'WIND';
+
+    default:
+      return 'ANY';
+  }
+  return 'ANY';
+}
+
 function prepareCurrentWeatherData(weatherData){
   if (weatherData && weatherData.currWeather )
   {
@@ -23,7 +93,8 @@ function prepareCurrentWeatherData(weatherData){
     return {
       time: ''+localDateTime(weatherData.currWeather.datetimeUnix, timezone),
       temperature: Math.round(weatherData.currWeather.temperature),
-      condition: weatherData.currWeather.weatherConditionDescr
+      condition: simplifyWeatherConditionId(weatherData.currWeather.weatherConditionId),
+      conditionDescr: weatherData.currWeather.weatherConditionDescr
     };
   }else{
     return {};
@@ -59,7 +130,7 @@ export default class CurrentWeather extends Component {
           <div>
             <h1>{data.temperature}</h1>
             <p>{data.time}</p>
-            <p>{data.condition}</p>
+            <p>{data.conditionDescr}</p>
           </div>
 
         }
--- src/containers/Outfits/Outfits.js
+++ src/containers/Outfits/Outfits.js
@@ -4,8 +4,8 @@ import {connect} from 'react-redux';
 import { asyncConnect } from 'redux-async-connect';
 import isEmpty from 'lodash.isempty';
 import {fetchCurrentWeather} from 'redux/modules/currentWeather';
-import {isLoaded, load as loadOutfits, changeOutfit} from 'redux/modules/outfits';
-import {CurrentOutfit, Forecast, CurrentWeather} from 'containers';
+import {load as loadOutfits, changeOutfit} from 'redux/modules/outfits';
+import {CurrentOutfit, CurrentWeather} from 'containers';
 import {ClothingItemList as ClothingItems} from 'components';
 import moment from 'moment-timezone';
 import coordinateTimezone from 'coordinate-tz';
@@ -20,6 +20,76 @@ function localDateTime(unixDateTime, timezone){
   const localTime = moment(unixDateTimeObj).tz(timezone).format('YYYY-MM-DD HH:mm:ss');
   return localTime;
 }
+function simplifyWeatherConditionId(weatherConditionId)
+{
+  const id = ''+ weatherConditionId;
+  switch (id) {
+    case '200':	//thunderstorm with light rain
+    case '201':	//thunderstorm with rain
+    case '202':	//thunderstorm with heavy rain
+    case '210':	//light thunderstorm
+    case '211':	//thunderstorm
+    case '212':	//heavy thunderstorm
+    case '221':	//ragged thunderstorm
+    case '230':	//thunderstorm with light drizzle
+    case '231':	//thunderstorm with drizzle
+    case '232':	//thunderstorm with heavy drizzle
+    case '300':	//light intensity drizzle
+    case '301':	//drizzle
+    case '302':	//heavy intensity drizzle
+    case '310':	//light intensity drizzle rain
+    case '311':	//drizzle rain
+    case '312':	//heavy intensity drizzle rain
+    case '313':	//shower rain and drizzle
+    case '314':	//heavy shower rain and drizzle
+    case '321': //shower drizzle
+    case '500':	//light rain
+    case '501':	//moderate rain
+    case '502':	//very heavy rain
+    case '504':	//extreme rain
+    case '511':	//freezing rain
+    case '520':	//light intensity shower rain
+    case '521':	//shower rain
+    case '522':	//heavy intensity shower rain
+    case '531':	//ragged shower rain
+    case '900':	//tornado
+    case '901':	//tropical storm
+    case '902':	//hurricane
+    case '906':	//hail
+    case '960':	//storm
+    case '961':	//violent storm
+    case '962':	//hurricane
+
+      return 'RAIN';
+
+    case '600':	//light snow
+    case '601':	//snow
+    case '602':	//heavy snow
+    case '611':	//sleet
+    case '612':	//shower sleet
+    case '615':	//light rain and snow
+    case '616':	//rain and snow
+    case '620':	//light shower snow
+    case '621':	//shower snow
+    case '622':	//heavy shower snow
+      return 'SNOW';
+
+    case '781':	//tornado
+    case '905':	//windy
+    case '954':	//moderate breeze
+    case '955':	//fresh breeze
+    case '956':	//strong breeze
+    case '957':	//high wind, near gale
+    case '958':	//gale
+    case '959':	//severe gale
+      return 'WIND';
+
+    default:
+      return 'ANY';
+  }
+
+  return 'ANY';
+}
 
 function prepareCurrentWeatherData(weatherData){
   if (weatherData && weatherData.currWeather )
@@ -28,7 +98,8 @@ function prepareCurrentWeatherData(weatherData){
     return {
       time: ''+localDateTime(weatherData.currWeather.datetimeUnix, timezone),
       temperature: Math.round(weatherData.currWeather.temperature),
-      condition: weatherData.currWeather.weatherConditionDescr
+      condition: simplifyWeatherConditionId(weatherData.currWeather.weatherConditionId),
+      conditionDescr: weatherData.currWeather.weatherConditionDescr
     };
   }else{
     return {};
@@ -60,10 +131,12 @@ function selectNextOutfit(outfits, currentOutfitIndex, changeOutfit)
   deferred: true,
   promise: ({store: {dispatch, getState}}) => {
     var globState = getState();
-    if (!isLoaded(globState)) {
-      //var currWeather = prepareCurrentWeatherData(globState.currentWeather.currentWeather);
-      return dispatch(loadOutfits({ temperature: -10, time: '2016-01-12 16:58:49', condition: 'SNOW' }, 'BUSINESS'));
+    var currWeather = prepareCurrentWeatherData(globState.currentWeather.currentWeather);
+    if(currWeather && currWeather.temperature && currWeather.time && currWeather.condition){
+      console.info('attempting to dipatch loadOutfitts');
+      return dispatch(loadOutfits({ temperature: currWeather.temperature, time: currWeather.time, condition: currWeather.condition }, 'BUSINESS'));
     }
+    //return dispatch(loadOutfits({ temperature: -10, time: '2016-01-12 16:58:49', condition: 'SNOW' }, 'BUSINESS'));
   }
 }])
 @connect(
